01bfd9deb18519d1a053604255265fab0a7010a0
Draminhon <britomurilorog@gmail.com>
2023-03-14 16:51:57 -0300

    Adicionando um novo elemento à implementação do nó

--- Implementação de nó/Testando.java
+++ Implementação de nó/Testando.java
@@ -9,15 +9,18 @@ public class Testando {
 		No_teste<String> no2 = new No_teste<>("conteúdo do No2");
 		No_teste<String> no3 = new No_teste<>("conteúdo do No3");
 		No_teste<String> no4 = new No_teste<>("conteúdo do No4");
+		No_teste<String> no5 = new No_teste<>("conteúdo do No5");
 		
 		no1.setProximo(no2);
 		no2.setProximo(no3);
 		no3.setProximo(no4);
+		no4.setProximo(no5);
 		
 		System.out.println(no1); //exibe o valor do primeiro nó
 		System.out.println(no1.getProximo()); //exibe o valor do segundo nó
 		System.out.println(no1.getProximo().getProximo()); //exibe o valor do terceiro nó
 		System.out.println(no1.getProximo().getProximo().getProximo()); //exibe o valor do quarto nó
+		System.out.println(no1.getProximo().getProximo().getProximo().getProximo());
 		
 
 	}
